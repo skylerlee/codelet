302026370ea7afa6378416c2766db6a05193ed32
skylerlee <skyler.ac.lee@gmail.com>
2017-09-19 00:24:42 +0800

    Add lanyon.js

---
+++ assets/js/lanyon.js
@@ -0,0 +1,58 @@
+/*
+ *  ___
+ * /\_ \
+ * \//\ \      __      ___   __  __    ___     ___
+ *   \ \ \   /'__`\  /' _ `\/\ \/\ \  / __`\ /' _ `\
+ *    \_\ \_/\ \_\.\_/\ \/\ \ \ \_\ \/\ \_\ \/\ \/\ \
+ *    /\____\ \__/.\_\ \_\ \_\/`____ \ \____/\ \_\ \_\
+ *    \/____/\/__/\/_/\/_/\/_/`/___/> \/___/  \/_/\/_/
+ *                               /\___/
+ *                               \/__/
+ *
+ * Designed, built, and released under MIT license by @skylerlee. Learn more at
+ * https://github.com/skylerlee/codelet.
+ */
+
+(function(exports) {
+  // mini css utils
+  var $css = {};
+
+  function _trim(str) {
+    return str.replace(/^[\s]+|[\s]+$/g, '');
+  }
+
+  function _getClass(el) {
+    return ' ' + _trim(el.className || '') + ' ';
+  }
+
+  function _setClass(el, className) {
+    el.className = _trim(className);
+  }
+
+  function hasClass(el, clz) {
+    return _getClass(el).indexOf(' ' + clz + ' ') > -1;
+  }
+
+  function addClass(el, clz) {
+    if (!hasClass(el, clz)) {
+      _setClass(el, _getClass(el) + clz);
+    }
+  }
+
+  function removeClass(el, clz) {
+    if (hasClass(el, clz)) {
+      _setClass(el, _getClass(el).replace(' ' + clz + ' ', ' '));
+    }
+  }
+
+  function toggleClass(el, clz) {
+    hasClass(el, clz) ? removeClass(el, clz) : addClass(el, clz);
+  }
+
+  $css.hasClass = hasClass;
+  $css.addClass = addClass;
+  $css.removeClass = removeClass;
+  $css.toggleClass = toggleClass;
+
+  exports.$css = $css;
+})(window);
