d873c2eed7a4f3ddbde4a407a5045f3c76e128ce
skylerlee <skyler.ac.lee@gmail.com>
2017-10-03 22:30:34 +0800

    Update script assets

--- assets/js/lanyon.js
+++ assets/js/lanyon.js
@@ -13,63 +13,19 @@
  * Learn more at https://github.com/skylerlee/codelet.
  */
 
-(function(exports) {
-  // mini css utils
-  var $css = {};
+import * as dom from './minidom';
 
-  function _trim(str) {
-    return str.replace(/^[\s]+|[\s]+$/g, '');
+// setup listeners
+var wrapper = document.getElementById('wrapper');
+var sidebar = document.getElementById('sidebar');
+var toggle = document.getElementById('sidebar-toggle');
+toggle.addEventListener('click', function () {
+  dom.toggleClass(toggle, 'active');
+  if (dom.hasClass(toggle, 'active')) {
+    dom.addClass(sidebar, 'open');
+    dom.addClass(wrapper, 'shift');
+  } else {
+    dom.removeClass(sidebar, 'open');
+    dom.removeClass(wrapper, 'shift');
   }
-
-  function _getClass(el) {
-    return ' ' + _trim(el.className || '') + ' ';
-  }
-
-  function _setClass(el, className) {
-    el.className = _trim(className);
-  }
-
-  function hasClass(el, clz) {
-    return _getClass(el).indexOf(' ' + clz + ' ') > -1;
-  }
-
-  function addClass(el, clz) {
-    if (!hasClass(el, clz)) {
-      _setClass(el, _getClass(el) + clz);
-    }
-  }
-
-  function removeClass(el, clz) {
-    if (hasClass(el, clz)) {
-      _setClass(el, _getClass(el).replace(' ' + clz + ' ', ' '));
-    }
-  }
-
-  function toggleClass(el, clz) {
-    hasClass(el, clz) ? removeClass(el, clz) : addClass(el, clz);
-  }
-
-  $css.hasClass = hasClass;
-  $css.addClass = addClass;
-  $css.removeClass = removeClass;
-  $css.toggleClass = toggleClass;
-
-  exports.$css = $css;
-})(window);
-
-(function() {
-  // setup listeners
-  var wrapper = document.getElementById('wrapper');
-  var sidebar = document.getElementById('sidebar');
-  var toggle = document.getElementById('sidebar-toggle');
-  toggle.addEventListener('click', function () {
-    $css.toggleClass(toggle, 'active');
-    if ($css.hasClass(toggle, 'active')) {
-      $css.addClass(sidebar, 'open');
-      $css.addClass(wrapper, 'shift');
-    } else {
-      $css.removeClass(sidebar, 'open');
-      $css.removeClass(wrapper, 'shift');
-    }
-  });
-})();
+});
---
+++ assets/js/minidom.js
@@ -0,0 +1,40 @@
+// minimal dom utils
+
+function _trim(str) {
+  return str.replace(/^[\s]+|[\s]+$/g, '');
+}
+
+function _getClass(el) {
+  return ' ' + _trim(el.className || '') + ' ';
+}
+
+function _setClass(el, className) {
+  el.className = _trim(className);
+}
+
+function hasClass(el, clz) {
+  return _getClass(el).indexOf(' ' + clz + ' ') > -1;
+}
+
+function addClass(el, clz) {
+  if (!hasClass(el, clz)) {
+    _setClass(el, _getClass(el) + clz);
+  }
+}
+
+function removeClass(el, clz) {
+  if (hasClass(el, clz)) {
+    _setClass(el, _getClass(el).replace(' ' + clz + ' ', ' '));
+  }
+}
+
+function toggleClass(el, clz) {
+  hasClass(el, clz) ? removeClass(el, clz) : addClass(el, clz);
+}
+
+export {
+  hasClass,
+  addClass,
+  removeClass,
+  toggleClass
+}
