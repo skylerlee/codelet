c9b921e8f2e242db185b2cf9106eac5619cafb6f
skylerlee <skyler.ac.lee@gmail.com>
2017-10-23 00:08:56 +0800

    Add editor style

--- _includes/js/leetcode_template.js
+++ _includes/js/leetcode_template.js
@@ -10,4 +10,12 @@ require([
 ], function (ace) {
   var srcBox = ace.edit('src-box');
   var outBox = ace.edit('out-box');
+
+  appendStyle(
+    '.ace-editor {\n' +
+    '  font-size: 16px;\n' +
+    '  height: 300px;\n' +
+    '  margin-bottom: 1rem;\n' +
+    '}'
+  );
 });
