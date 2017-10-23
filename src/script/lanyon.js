f479c1b695bf9548d2460adbe972ece6e1d07942
skylerlee <skyler.ac.lee@gmail.com>
2017-10-24 00:02:36 +0800

    Add Date format method

--- src/script/lanyon.js
+++ src/script/lanyon.js
@@ -43,3 +43,40 @@ window.appendStyle = function (text) {
   style.innerHTML = text;
   document.head.appendChild(style);
 };
+
+function _getPad(str, width) {
+  width = width >> 0;
+  var len = width - str.length;
+  var pad = '';
+  if (len < 1) {
+    return pad;
+  }
+  while (len--) {
+    pad = pad + '0';
+  }
+  return pad;
+}
+
+window.Date.format = function (str) {
+  var datetime = new Date();
+  var ctx = {
+    Y: '' + datetime.getFullYear(),
+    M: '' + datetime.getMonth() + 1,
+    D: '' + datetime.getDate(),
+    h: '' + datetime.getHours(),
+    m: '' + datetime.getMinutes(),
+    s: '' + datetime.getSeconds()
+  };
+  return str.replace(/%(\d*)([YMDhms])/g, function (mat, digit, key) {
+    var value = ctx[key];
+    if (digit !== '') {
+      var width = parseInt(digit);
+      if (width < value.length) {
+        return value.substr(0, width);
+      } else {
+        return _getPad(value, width) + value;
+      }
+    }
+    return value;
+  });
+};
