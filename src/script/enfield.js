221d1d3256ade52fd0e95e8eb0592cde0288d850
skylerlee <skyler.ac.lee@gmail.com>
2017-11-18 00:49:48 +0800

    Fix blog page bugs

--- src/script/enfield.js
+++ src/script/enfield.js
@@ -16,6 +16,8 @@
 import dom from './minidom';
 import utils from './utils';
 
+window.$utils = utils;
+
 // setup listeners
 var wrapper = document.getElementById('wrapper');
 var sidebar = document.getElementById('sidebar');
@@ -33,31 +35,31 @@ dom(toggle).on('click', function () {
   }
 });
 
-var page = {
-  min: Number(cursor.getAttribute('min')),
-  max: Number(cursor.getAttribute('max')),
-  gotoPage: function (num) {
-    var path = location.pathname;
-    var dest = path;
-    if (/page\d+\/?$/.test(path)) { // page postfix
-      dest = path.match(/^(.*\/)page\d+\/?$/)[1];
+if (cursor) {
+  var page = {
+    min: Number(cursor.getAttribute('min')),
+    max: Number(cursor.getAttribute('max')),
+    gotoPage: function (num) {
+      var path = location.pathname;
+      var dest = path;
+      if (/page\d+\/?$/.test(path)) { // page postfix
+        dest = path.match(/^(.*\/)page\d+\/?$/)[1];
+      }
+      if (num > 1) {
+        dest += 'page' + num + '/';
+      }
+      document.location = dest;
     }
-    if (num > 1) {
-      dest += 'page' + num + '/';
-    }
-    document.location = dest;
-  }
-};
+  };
 
-dom(cursor).on('keypress', function (e) {
-  if (e.charCode === 13) {
-    var val = parseInt(cursor.value);
-    if (val >= page.min && val <= page.max) {
-      page.gotoPage(val);
-    } else {
-      dom(cursor).addClass('invalid');
+  dom(cursor).on('keypress', function (e) {
+    if (e.charCode === 13) {
+      var val = parseInt(cursor.value);
+      if (val >= page.min && val <= page.max) {
+        page.gotoPage(val);
+      } else {
+        dom(cursor).addClass('invalid');
+      }
     }
-  }
-});
-
-window.$utils = utils;
+  });
+}
