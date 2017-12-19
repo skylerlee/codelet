aec83736ea40eada2f084e72736ca1deca9cb301
skylerlee <skyler.ac.lee@gmail.com>
2017-12-19 18:23:14 +0800

    Add logging utility

---
+++ logger/logger.h
@@ -0,0 +1,30 @@
+// console logger utility
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+#include <iostream>
+
+namespace clogger {
+
+class Logger {
+ public:
+  Logger() = default;
+  Logger(const Logger&) = delete;
+  ~Logger() = default;
+  Logger& operator=(const Logger&) = delete;
+
+  template<typename T, typename ...Args>
+  void log(T t, Args... args) {
+    std::cout << t;
+    log(args...);
+  }
+
+  template<typename T>
+  void log(T t) {
+    std::cout << t << std::endl;
+  }
+};
+
+}  // namespace clogger
+
+#endif  // LOGGER_H_
